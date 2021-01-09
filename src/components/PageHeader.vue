1f9e8cf9c64cc9c0df98eca50e91e6e82934d088
akaKAIN <mkain@mail.ru>
2021-01-09 22:44:38 +1000

    Connect fa-icons. Connect google-font. Finished preliminary header.

--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -1,12 +1,17 @@
 <template>
-  <header>
+  <header id="header">
     <div class="user-wrapper">
-      <div class="user-name" v-if="userName && isLogin">{{ userName }}</div>
+      <template v-if="userName && isLogin">
+        <span class="user-name">{{ userName }}</span>
+        <i class="fas fa-door-open"></i>
+      </template>
       <span v-else>Гость</span>
     </div>
-    <div class="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">Route#1</router-link>
+    <div class="nav-wrapper">
+      <div class="container">
+        <router-link class="nav-item" to="/">Home</router-link>
+        <router-link class="nav-item" to="/about">Route#1</router-link>
+      </div>
     </div>
   </header>
 </template>
@@ -28,5 +33,37 @@ export default {
 </script>
 
 <style scoped lang="scss">
+$container-size: 800px;
+#header {
+  .user-wrapper {
+    text-align: right;
+    padding: 10px 50px;
+    @media (max-width: $container-size) {
+      padding: 10px 20px;
+    }
 
+    .user-name {
+    }
+  }
+
+  .nav-wrapper {
+    text-align: right;
+    padding: 10px;
+    background-color: #1A8237;
+
+    .nav-item {
+      color: #ffffff;
+      margin: 0 10px;
+    }
+  }
+}
+
+.container {
+  max-width: $container-size;
+  margin: 0 auto;
+}
+
+.ml-1 {
+  margin-left: 1rem;
+}
 </style>
