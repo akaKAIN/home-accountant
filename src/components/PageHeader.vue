61757498156021a1cc2bde9e5cc44129fa4de9e6
akaKAIN <mkain@mail.ru>
2021-01-09 11:28:03 +1000

    Init base styles. Init header-component

---
+++ src/components/PageHeader.vue
@@ -0,0 +1,32 @@
+<template>
+  <header>
+    <div class="user-wrapper">
+      <div class="user-name" v-if="userName && isLogin">{{ userName }}</div>
+      <span v-else>Гость</span>
+    </div>
+    <div class="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">Route#1</router-link>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'PageHeader',
+  props: {
+    userName: {
+      type: String,
+      default: ''
+    },
+    isLogin: {
+      type: Boolean,
+      default: false
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
